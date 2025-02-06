2bd4cb48d5437d1c6c5c10dab41ffda2f4319d03
ANISH-422004 <149455330+ANISH-422004@users.noreply.github.com>
2025-02-06 12:29:49 +0530

    Time: 0 ms (100%), Space: 50.8 MB (32.97%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -2,7 +2,7 @@
 // //BRUTE
 // var checkIfExist = function(arr) {
 //     let n = arr.length;
-    
+
 //     for (let i = 0; i < n; i++) {
 //         for (let j = 0; j < n; j++) {
 //             if (i !== j && arr[i] === 2 * arr[j]) {
@@ -14,21 +14,47 @@
 //     return false;
 // };
 
-//OPTIMIZED {in class test i did it }
+
+
+
+// //OPTIMIZED {in class test i did it }
+// var checkIfExist = function (arr) {
+//     let set = new Set()
+
+//     for (let i = 0; i < arr.length; i++) {
+//         set.add(arr[i])
+//         if (arr[i] != 0) if (set.has(arr[i] / 2) || set.has(arr[i] * 2)) return true
+
+//     }
+//     let c = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] == 0) c++
+//         if (c > 1) return true
+//     }
+
+//     return false
+
+// };
+
+
+
+
+
+// using MAP O(n)   no need of checking sero differnetly
 var checkIfExist = function (arr) {
-    let set = new Set()
+    let map = new Map();
 
     for (let i = 0; i < arr.length; i++) {
-        set.add(arr[i])
-        if (arr[i] != 0) if (set.has(arr[i] / 2) || set.has(arr[i] * 2)) return true
+        let num = arr[i];
 
+        // Check if 2 * num or num / 2 already exists in the map
+        if (map.has(num * 2) || (num % 2 === 0 && map.has(num / 2)) && i != map.get(arr[i])  ) {
+            return true;
+        }
+
+        // Store the current number in the map
+        map.set(num, i);
     }
-    let c = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == 0) c++
-        if (c > 1) return true
-    }
 
-    return false
-
+    return false;
 };
