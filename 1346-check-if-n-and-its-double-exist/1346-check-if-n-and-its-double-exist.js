6fb3388b2c2b20928ba89149b56fff624179b1d1
ANISH-422004 <149455330+ANISH-422004@users.noreply.github.com>
2025-02-06 12:04:56 +0530

    Time: 0 ms (100%), Space: 50.3 MB (57.32%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -1,18 +1,32 @@
-/**
- * @param {number[]} arr
- * @return {boolean}
- */
+
+// //BRUTE
+// var checkIfExist = function(arr) {
+//     let n = arr.length;
+    
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n; j++) {
+//             if (i !== j && arr[i] === 2 * arr[j]) {
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// };
+
+//OPTIMIZED {in class test i did it }
 var checkIfExist = function (arr) {
     let set = new Set()
-    let c= 0 
-    for (let i = 0 ; i<arr.length ; i++ ){
-        if(arr[i]==0) c++
-        if(c>1) return true
-    }
-    for (let i = 0 ; i<arr.length ; i++) {
-        set.add(arr[i])
-        if(arr[i]!=0) if(set.has(arr[i]/2) || set.has(arr[i]*2)) return true
 
+    for (let i = 0; i < arr.length; i++) {
+        set.add(arr[i])
+        if (arr[i] != 0) if (set.has(arr[i] / 2) || set.has(arr[i] * 2)) return true
+
+    }
+    let c = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) c++
+        if (c > 1) return true
     }
 
     return false
