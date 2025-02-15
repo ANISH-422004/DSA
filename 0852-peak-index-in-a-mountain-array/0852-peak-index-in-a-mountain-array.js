430c4ca972b562f69111bd4d955f9803c6321e47
ANISH-422004 <149455330+ANISH-422004@users.noreply.github.com>
2025-02-16 01:09:24 +0530

    Time: 0 ms (100%), Space: 60.1 MB (14.55%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -8,9 +8,12 @@ var peakIndexInMountainArray = function (arr) {
     let ans = 0;
     while (s <= e) {
         let mid = Math.floor((s + e) / 2)
-        if (arr[mid] < arr[mid + 1]) { ans = mid + 1; s = mid + 1; }
-        else if (arr[mid] > arr[mid + 1]) e = mid - 1
-        else s = mid + 1
+        if (arr[mid] < arr[mid + 1]) {
+            ans = mid + 1;
+            s = mid + 1;
+        }
+
+        else e = mid - 1
     }
     return ans;
 };
