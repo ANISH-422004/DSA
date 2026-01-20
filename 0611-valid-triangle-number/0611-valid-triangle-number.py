261a5b0da1ae3f5ab3dccf04312a3212bc4eb067
ANISH-422004 <149455330+ANISH-422004@users.noreply.github.com>
2026-01-20 22:36:41 +0530

    Time: 458 ms (36.36%), Space: 19.3 MB (17.63%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,34 +1,55 @@
-def bs(nl, left, right, target):
-    ans = left - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nl[mid] < target:
-            ans = mid
-            left = mid + 1
-        else:
-            right = mid - 1
-    return ans
+# def bs(nl, left, right, target):
+#     ans = left - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if nl[mid] < target:
+#             ans = mid
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return ans
+
+
+# class Solution:
+#     def triangleNumber(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n < 3:
+#             return 0
+
+#         nl = sorted(nums)
+#         count = 0
+
+#         for i in range(0, n - 2):
+#             if nl[i] == 0:
+#                 continue
+
+#             for j in range(i + 1, n - 1):
+#                 s = nl[i] + nl[j]
+
+#                 k = bs(nl, j + 1, n - 1, s)
+
+#                 if k > j:
+#                     count += (k - j)
+
+#         return count
+
 
 
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
         n = len(nums)
-        if n < 3:
-            return 0
-
-        nl = sorted(nums)
         count = 0
 
-        for i in range(0, n - 2):
-            if nl[i] == 0:
-                continue
+        for k in range(n - 1, 1, -1):
+            i = 0
+            j = k - 1
 
-            for j in range(i + 1, n - 1):
-                s = nl[i] + nl[j]
-
-                k = bs(nl, j + 1, n - 1, s)
-
-                if k > j:
-                    count += (k - j)
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    count += (j - i)
+                    j -= 1
+                else:
+                    i += 1
 
         return count
